d74326e62aade3365158aef2009f52f84eab7f8d
Thrace Bebe <thracebe@va-b5.kzn.21-school.ru>
2023-07-11 20:17:34 +0300

    added header

--- src/header.h
+++ src/header.h
@@ -8,84 +8,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <regex.h>
+#include <stdbool.h>
 #include <ctype.h>
+#include <math.h>
 
 # define SIZE 260
 
-
-typedef struct trigon {
-    int cos, sin, tan, acos, asin, atan, sqrt, ln, log;
-} trigon_n;
-
-typedef struct symbol {
-    int plus, minus, multiply, division, interest, bracket, unbracket, degree, dot;
-} symbol_n;
-
-typedef struct t_stack {
-    char value;
-    int priority;
+typedef struct t_stack{
+    char* value;
     struct t_stack* next;
 } t_stack;
 
-typedef struct queue {
-    int value;
-    struct queue* next;
-} queue;
 
 
-
-//for stack
-t_stack* create_stack_elem(char value);
-void print_all_stack(t_stack** head);
-void print_one_elem_from_stack(t_stack** head);
-void add_elem_to_stack(t_stack** head, t_stack* elem);
-void delete_elem_from_stack(t_stack** head);
-int check_trigon(char *bukva, int *dot, trigon_n *tr);
-int check_symbol(char *bukva, int dot, symbol_n *sy);
-int get_priority(char symbol);
-//for queue
-// void enqueue(int);
-// void dequeue();
-// void show();
-
-
-
+bool is_empty(t_stack *head);
+void print_all_stack(t_stack* head);
+t_stack* create_stack_elem(char* value);
+int is_operator(char *symbol, char *str, int *i);
+int getPriority(char* symbol);
+void push_front(t_stack** head, t_stack* elem);
+char* get_top_elem(t_stack* head);
+bool check_trigon(char* substr);
+void push_back(t_stack **head, t_stack *elem);
+t_stack* pop(t_stack** head);
+bool is_unary_operator(const char* operator);
+char* get_last_elem(t_stack* head);
+t_stack* pop_back(t_stack** head);
+int get_number(char * infix, int pos, t_stack **head);
+void double_to_str(double x, t_stack **head);
+void infixToPostfix(char* infix, t_stack** numbers);
+double evaluatePostfix(t_stack** postfix);
 
 
 
 #endif //C7_SMARTCALC_V1_0_1_HEADER_H_
-
-
-
-
-
-
-
-
-
-
-// Высший приоритет:
-
-// ( и ): скобки для управления порядком операций
-// Средний приоритет:
-
-// ^: возведение в степень
-// Низший приоритет:
-
-// * и /: умножение и деление
-// %: остаток от деления
-
-1 2 + 1 3 * 3 / - 3 3 / 1 2 - * +
-
-
-
-
-
-
-
-
-
-
-
 
